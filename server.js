951a263d0c647e52055736cf74974b32bd7bfba8
LBJ <mac@macbookui-MacBook-Pro.local>
2018-09-01 11:40:16 +0900

    google api key

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const http = require('http');
+require('dotenv').config();
 
 const server = http.createServer((req, res) => {
     if(req.method == 'GET' && req.url == '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write(`
-        <html>
+        <!DOCTYPE html>
         <head>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap"></script>
         <body>
-        Hello LandLord!
+            Hello LandLord!
         </body>
+        <style>
+            #map {
+                height: 100%;
+            }
+            html, body {
+                height: 100%;
+                margin: 0;
+                padding: 0;
+            }
+        </style>
         </head>
         </html>
         `);
